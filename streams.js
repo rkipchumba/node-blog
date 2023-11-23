60f8fbe08540b21bf434c5c8af092112cb90dc2e
Rodgers Kipchumba <kipchumbarodgers@gmail.com>
2023-11-23 13:28:38 +0300

    added piping

--- streams.js
+++ streams.js
@@ -5,9 +5,13 @@ const readStream = fs.createReadStream("./docs/blog3.txt", {
 });
 const writeStream = fs.createWriteStream("./docs/blog4.txt");
 
-readStream.on("data", (chunk) => {
-  console.log("------new chank ----");
-  console.log(chunk.toString());
-  writeStream.write("\nNew Chunk\n");
-  writeStream.write(chunk);
-});
+// readStream.on("data", (chunk) => {
+//   console.log("------new chank ----");
+//   console.log(chunk.toString());
+//   writeStream.write("\nNew Chunk\n");
+//   writeStream.write(chunk);
+// });
+
+
+// piping
+readStream.pipe(writeStream)
