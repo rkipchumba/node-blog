85a35b4b9b72381d237ccd1e3872f2c49dab435d
Rodgers Kipchumba <kipchumbarodgers@gmail.com>
2023-11-27 13:18:56 +0300

    output docs in views

--- app.js
+++ app.js
@@ -24,58 +24,25 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
-// mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res) => {
-  const blog = new Blog({
-    title: 'new blog 2',
-    snippet: 'about my new blog',
-    body: 'More about my blog'
-  });
 
-  blog.save()
-    .then((result) => {
-      console.log('Blog saved:', result);
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log('Error saving blog:', err);
-      res.status(500).send('Internal Server Error');
-    });
-});
-
-app.get('/all-blogs', (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-})
-
-
-app.get('/single-blog', (req, res) => {
-  Blog.findById('65646764c399f9756ae10e4f')
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-})
-
+// routes
 app.get('/',  (req, res) => {
-  const blogs = [
-    {title: 'Kev says something', snippet: 'Giggdy Giggdy Giggdy '},
-    {title: 'Kev says something', snippet: 'Giggdy Giggdy Giggdy '},
-    {title: 'Kev says something', snippet: 'Giggdy Giggdy Giggdy '}
-  ]
-  res.render('index', { title: "Home", blogs })
+  res.redirect('/blogs');
   })
 
 app.get('/about',  (req, res) => {
-  // res.send('<h1>About page</h1>')
   res.render('about', { title: "About" })
+})
+
+// blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', {title: 'All Blogs', blogs: result })
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 })
 
   // redirects
