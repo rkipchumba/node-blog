ec33fbb2da2f7c7e5ae9265a123cdd8f4b0dd3d4
Rodgers Kipchumba <kipchumbarodgers@gmail.com>
2023-11-27 11:21:00 +0300

    3rd party middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 
 // express app
 const app = express()
@@ -8,6 +9,8 @@ app.set('view engine', 'ejs');
 
 // listen for requests
 app.listen(3000)
+
+app.use(morgan('dev'));
 
 app.get('/',  (req, res) => {
   const blogs = [
